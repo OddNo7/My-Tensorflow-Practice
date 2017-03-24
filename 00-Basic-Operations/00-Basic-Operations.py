6f6976295c36978129358ab19369a82d06a81b35
OddNo7 <q1995322@icloud.com>
2017-03-23 23:43:31 -0400

    00 and 01
    Inlucdes TF basic operations: placeholder, variable, loss, session
    A practice on k-NN classifier on MNIST dataset

---
+++ 00-Basic-Operations/00-Basic-Operations.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import numpy as np
+
+# Constant addition, reduction, multiplication and division
+a = tf.constant(3, dtype=tf.float32, name='a')
+b = tf.constant(4, dtype=tf.float32, name='b')
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+# Run the computational graph
+sess = tf.Session()
+print(sess.run([c, d, e, f]))
+
+# A placeholder must be fed with data. On definition of a place holder dtype must be specified. One can also specify name and shape.
+# The feeding usually goes with 'feed_dict' argument.
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+with tf.Session() as sess:
+    print(sess.run(a + b, feed_dict={a: 3, b: 4}))
+
+# A variable is a value that can be trained during the running of a graph.
+# A variable needs to be designated an initial value when created. On
+# computation, this initial value need to be assigned as a initialization
+# process.
+W = tf.Variable(2.0, tf.float32, name='W')
+b = tf.Variable(3.5, tf.float32, name='b')
+x = tf.placeholder(tf.float32, name='x')
+line = W * x + b
+init = tf.global_variables_initializer()
+# For initialization, one can call a variable's method 'initializer' to
+# initiate the value, or use global initializer.
+
+with tf.Session() as sess:
+    sess.run(init)
+    print(sess.run(line, feed_dict={x: [1, 2, 3, 4]}))
+
+# Loss function. To evaluate how good a model is, we need to find a loss function,
+# With loss function we can try to train the variables so that the model
+# fits the fed data.
+y = tf.placeholder(tf.float32, name='y')
+with tf.name_scope('Loss'):
+    MSE = tf.reduce_mean(tf.square(line - y), name='MSE')
+with tf.name_scope('SGD'):
+    optimizer = tf.train.GradientDescentOptimizer(0.02).minimize(MSE)
+
+
+xtr = np.array([1.0, 2.0, 3.0, 4.0])
+ytr = np.array([2.0, 3.0, 4.0, 5.0])
+# Now one needs to minimize the loss function. To do that, we need to call some optimizer.
+# tf.train provides several optimizers. We can use the basic one and build
+# an instance, and call its minimize method.
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    for epoch in range(2000):
+        if epoch % 100 == 0:
+            print('Reaching {}00-th epoch'.format(epoch // 100))
+            print(sess.run(MSE, feed_dict={x: xtr, y: ytr}))
+        sess.run(optimizer, feed_dict={x: xtr, y: ytr})
+    print(sess.run([W, b]))
---
+++ 01-kNN-Classifier/01-kNN-classifier.py
@@ -0,0 +1,46 @@
+import numpy as np
+import tensorflow as tf
+import os
+from tensorflow.examples.tutorials.mnist import input_data
+
+# Specify a subset of MNIST as training set and test set
+test_size = 400
+train_size = 20000
+# Since MNIST contains 10 categories, use a bigger K could possibly reach
+# a better output.
+K = 13
+
+# Load data
+mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+Xtr, Ytr = mnist.train.next_batch(train_size)
+Xte, Yte = mnist.test.next_batch(test_size)
+
+# Each time compute Euclidean distance across the training set
+xtr = tf.placeholder(tf.float32, shape=[None, 784])
+xte = tf.placeholder(tf.float32, shape=[784])  # Feed one test sample each time
+ytr = tf.placeholder(tf.float32, shape=[None, 10])
+
+# Calculate Eauclidean distance for each dimension, then sum up
+distance = tf.reduce_sum(tf.square(xtr - xte), reduction_indices=1)
+_, ind = tf.nn.top_k(-distance, k=K)  # Fine indices of top K neighbors
+
+nearest_neighbor = []
+for i in range(K):
+    nearest_neighbor.append(ytr[ind[i], :])
+# Voting: Each neighbor vote for a class. Summing their ground truths
+# therefore helps predict the test's label.
+# Gather the y-label of these neighbors and sum up. Find argmax.
+kneighbors = tf.transpose(tf.pack(nearest_neighbor, axis=1))
+pred = tf.argmax(tf.reduce_sum(kneighbors, reduction_indices=0), axis=0)
+
+count = 0
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for index in range(len(Xte)):
+        inpt = Xte[index, :]
+        label = sess.run(pred, feed_dict={xtr: Xtr, ytr: Ytr, xte: inpt})
+        print('{}-th test, pred={}, truth={}'.format(index,
+                                                     label, np.argmax(Yte[index])))
+        if label == np.argmax(Yte[index]):
+            count += 1
+    print('Total accuracy: {:f}%.'.format(count / test_size * 100))
